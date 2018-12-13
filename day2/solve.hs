a8c1b681db69e6fb432ea8872ddf75ae5a4dc66b
Bruno Bieth <biethb@gmail.com>
2018-12-13 13:26:08 +0100

    day 2

---
+++ day2/solve.hs
@@ -0,0 +1,18 @@
+import System.IO
+import qualified Data.Map as Map
+import qualified Data.List as List
+import qualified Data.Maybe as Maybe
+import Debug.Trace (trace)
+
+checksum = do
+    f <- readFile "input"
+    let (doubles, triples) = foldl countLine (0,0) (lines f)          
+        countLine doubleTriple line =
+            let (double, triple) = doubleTriple
+                freqs = Map.fromListWith (+) $ zip line (repeat 1)
+            in (double + (countOccurrence freqs 2), triple + (countOccurrence freqs 3)) -- trace ("freqs = " ++ (show freqs)) 
+            where
+                countOccurrence freqs num = Maybe.fromMaybe 0 (fmap (\_ -> 1) (List.find (\l -> snd l == num) (Map.toList freqs)))
+    print (doubles * triples)
+    
+            
