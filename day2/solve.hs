0bd85741fb21c2adf68d08309306af570fd69088
Bruno Bieth <biethb@gmail.com>
2018-12-13 17:18:14 +0100

    day 2, part 2

--- day2/solve.hs
+++ day2/solve.hs
@@ -4,6 +4,8 @@ import qualified Data.List as List
 import qualified Data.Maybe as Maybe
 import Debug.Trace (trace)
 
+import Combinatorics (tuples)
+
 checksum = do
     f <- readFile "input"
     let (doubles, triples) = foldl countLine (0,0) (lines f)          
@@ -15,4 +17,10 @@ checksum = do
                 countOccurrence freqs num = Maybe.fromMaybe 0 (fmap (\_ -> 1) (List.find (\l -> snd l == num) (Map.toList freqs)))
     print (doubles * triples)
     
-            
+-- part 2
+commonLetters = do
+    f <- readFile "input"            
+    let combinations = tuples 2 (lines f)
+        sameLetters lst = map fst $ filter (\a -> (fst a) == (snd a)) (zip (head lst) (last lst))
+        length' a b = compare (length a) (length b)
+    print (List.maximumBy length' (map sameLetters combinations))
